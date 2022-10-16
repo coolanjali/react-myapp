44f233c5b18b637b1c8189aef416e5c7b35e6bac
coolanjali <112107778+coolanjali@users.noreply.github.com>
2022-10-16 17:06:00 +0200

    added weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
        <h1>Find the city Weather🔎🔎🔎</h1>
+       <Weather/>
     </div>
   );
 }
