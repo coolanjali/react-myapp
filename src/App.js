33b2d01f13d663ce5389860e54db8c529456f858
coolanjali <112107778+coolanjali@users.noreply.github.com>
2022-10-16 17:09:27 +0200

    added connection

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
        <h1>Find the city Weather🔎🔎🔎</h1>
        <Weather/>
+       <p>Coded by Priyanka in <a href="https://github.com/coolanjali/react-myapp">Github</a></p>
     </div>
+
   );
 }
 
